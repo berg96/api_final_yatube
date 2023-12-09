b81b6b8c2461dcfa942ec604c574f5e7addf17dd
Artem Kulikov <chigar2010@yandex.ru>
2023-12-09 22:59:22 +0200

    some fixes

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import AuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
-from posts.models import Comment, Follow, Group, Post, User
+from posts.models import Group, Post
 
 
 class CreateListViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
